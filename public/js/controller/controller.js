00ac3b2be1860f7e12d604483b9a0a4ea12fddb4
farbizu <fiorellaarbizu@gmail.com>
2016-11-11 19:16:54 -0600

    Lista de cursos
    Se agrega funcionalidad de desplegar u ocultar un curso al hacerle click

--- public/js/controller/controller.js
+++ public/js/controller/controller.js
@@ -1,54 +1,52 @@
 var app = angular.module("myApp", []);
 app.controller("AppCtrl", ['$scope', '$http', function ($scope, $http) {
 
-	var userData = function() {
-		$http.get('/app/userData').success(function(response) {
-			$scope.username = response.login;
-            $scope.fname= response.fname;
+    var userData = function () {
+        $http.get('/app/userData').success(function (response) {
+            $scope.username = response.login;
+            $scope.fname = response.fname;
             $scope.lname = response.lname;
-		});
-	};
-
-	userData();
-
-	$scope.logout = function() {
-		$scope.logout = "/";
-        
-		$http.get('/app/logout').success(function(response) {
-			$scope.username = "";
-            $scope.fname ="";
-            $scope.lname = "";
-		});
-	};
-    
-    $scope.listaCursos = function(){
-        
-        $http.get('/app/listaCursos').success(function(response){
-            $scope.listaCursos = response;
-            
         });
-        
-        
     };
-    
-    $scope.esconde = function(){
-        
-   /*for (var i = 0, i < $scope.listaCursos.length; i++) {
-     
-       document.getElementById(i.code).addClass(".hide");
-     
-    }  */ 
+
+    userData();
+
+    $scope.logout = function () {
+        //$scope.logout = "/";
+
+        $http.get('/app/logout').success(function (response) {
+            $scope.logout = "/";
+            $scope.username = "";
+            $scope.fname = "";
+            $scope.lname = "";
+        });
+    };
+
+    $scope.listaCursos = function () {
+
+        $http.get('/app/listaCursos').success(function (response) {
+            $scope.listaCursos = response;
+            $scope.esconde();
+        });
+
 
     };
-    
-    
-    $scope.despliega = function(code){
-       // $scope.esconde();
+
+       $scope.desplegar = function (code) {
+      
+        
+        if ($('#' + code).is(":hidden")) {
+            
+            
+             $('#' + code).removeAttr("style");
+            
+        } else {
+           $('#' + code).attr("style", "display:none");
+        }
         
         
-        
-    }
-    
-    
-       
+    };
+
+
+
 }]); //final controller
